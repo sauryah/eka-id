e3feb1624968921f8c8a0af92d34325fbf2f8307
sahil <sauryahjoshi@gmail.com>
2026-09-16 09:37:18 +0530

    fix(web): resolve useEffect exhaustive-deps warning in admin console

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -106,6 +106,7 @@ export default function AdminPage() {
 
   useEffect(() => {
     loadAdminData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const loadAdminData = async () => {
